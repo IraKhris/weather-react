f8a1888d04df1f6edac984cc2bcc54a4352696cb
IraKhris <115898423+IraKhris@users.noreply.github.com>
2023-01-22 18:33:29 -0700

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ function App() {
         <h1>Weather Search Engine</h1>
         <Search />
       </div>
-      <div>Open Sourced by Iryna Khrystych</div>
+      <br />
+      <div>
+        <a
+          href="https://github.com/IraKhris/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open Sourced{" "}
+        </a>
+        by{" "}
+        <a
+          href="https://gregarious-sprinkles-cd236b.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Iryna Khrystych
+        </a>
+      </div>
     </div>
   );
 }
